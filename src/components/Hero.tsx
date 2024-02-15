402fe5f012e85f7f10a2f4ca64a6331351f3162c
skupta12 <tosya2001@gmail.com>
2024-02-16 00:14:27 +0200

    optimizing mobile gradients

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 const Hero = () => {
 
   return (
-    <section className="lg:pb-[200px] pb-16 overflow-hidden bg-no-repeat md:bg-center bg-top gradient">
+    <section className="lg:pb-[200px] pb-16 overflow-hidden bg-no-repeat md:bg-center gradient gradient-mobile">
       <MaxWidthWrapper>
         <div className="lg:pt-[230px] pt-[150px] relative">
           <div className="grid grid-cols-12 gap-y-12 lg:gap-y-7 gap-x-5 lg:place-items-center">
