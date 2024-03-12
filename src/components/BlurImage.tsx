d05e4a31e63ab782ae08dda8e114c6cb2e339e23
skupta12 <tosya2001@gmail.com>
2024-03-12 16:07:36 +0200

    image issues v1.5

--- src/components/BlurImage.tsx
+++ src/components/BlurImage.tsx
@@ -35,7 +35,7 @@ const BlurImage = ({
       src={src}
       className={`${className} ${
         zoomIn && "hover:scale-110"
-      } ${isLoading ? "blur-1xl" : "blur-0"}
+      } ${isLoading ? "blur-[20px]" : "blur-0"}
      `}
       fill={fill}
       onLoad={() => setIsLoading(false)}
