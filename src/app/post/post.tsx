d872519e4643b27606bb722cec126cb4f57c59b8
skupta12 <tosya2001@gmail.com>
2024-03-13 12:54:08 +0200

    image issues v2.4

--- src/app/post/post.tsx
+++ src/app/post/post.tsx
@@ -24,7 +24,7 @@ export default async function Post({
       {blogs?.map(({ id, url, src, label, text, date, blur }) => (
         <div key={id} className="lg:col-span-4 md:col-span-6 col-span-12">
           <Link className="relative" href={`/post/${url}`}>
-            <div className="relative overflow-hidden h-[650px]">
+            <div className="relative overflow-hidden h-[260px]">
               <Image
                 className="object-cover"
                 fill
