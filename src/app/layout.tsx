a587a087dddf91d4d39ae946c30972be17cd5426
skupta12 <tosya2001@gmail.com>
2024-05-10 17:16:26 +0300

    --

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,24 @@
-"use client"
-
 import { inter } from "./fonts";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { constructMetadata } from "@/lib/utils";
-import { MotionConfig } from "framer-motion";
 
-// export const metadata = constructMetadata();
+export const metadata = constructMetadata()
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Header />
         <main className="relative flex flex-col">
           <div className="flex-grow flex-1">
-            <MotionConfig reducedMotion="user">{children}</MotionConfig>
+            {children}       
           </div>
         </main>
         <Footer />
