a7ae1a4932c1253ab4bd06d4f86b7b2005a147c4
skupta12 <tosya2001@gmail.com>
2024-05-10 17:06:13 +0300

    testing reducedMotion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,19 @@ import Portfolio from "@/sections/Portfolio";
 import Services from "@/sections/Services";
 import Testimonial from "@/sections/Testimonial";
 import Trust from "@/sections/Trust";
+import { MotionConfig } from "framer-motion";
 
 export default function Home() {
-
   return (
-    <>
+    <MotionConfig reducedMotion="user">
       <Hero />
       <Trust />
       <Services />
       <Portfolio />
-      {/* <Testimonial /> */}
       <Testimonial />
       <Ai />
       <Blog />
-      <CTA />  
-    </>
+      <CTA />
+    </MotionConfig>
   );
 }
